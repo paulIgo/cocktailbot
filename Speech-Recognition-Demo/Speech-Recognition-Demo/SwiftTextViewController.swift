a0e1e781317b25b17d3d4ce7de4c3f7b03fb53d7
Paul Igo <35532313+paulIgo@users.noreply.github.com>
2019-10-25 12:37:19 -0400

    Updated for case A and B

---
+++ Speech-Recognition-Demo/Speech-Recognition-Demo/SwiftTextViewController.swift
@@ -0,0 +1,62 @@
+import UIKit
+import Speech
+
+//https://medium.com/ios-os-x-development/speech-recognition-with-swift-in-ios-10-50d5f4e59c48
+//Adapted Source Tutorial: https://github.com/jen2/Speech-Recognition-Demo
+
+class SwiftTextViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var textHolder: UILabel!
+    @IBOutlet weak var inputString: UITextField!
+    @IBOutlet weak var SubmitWritten: UIButton!
+    
+    
+    
+    var masterTextList:[String] = [];
+    
+    override func viewDidLoad() {
+        masterTextList = []
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func OnClickSubmit(_ sender: Any) {
+        var text = inputString.text
+        
+        masterTextList = []
+        masterTextList = (text?.components(separatedBy: ","))!
+        
+        //https://stackoverflow.com/questions/28548908/how-to-remove-whitespaces-in-strings-in-swift
+        masterTextList = masterTextList.map { $0.trimmingCharacters(in: .whitespaces) }
+        masterTextList = masterTextList.map { $0.lowercased()}
+        showPossibleDrinks();
+    }
+    
+    
+    func showPossibleDrinks(){
+        textHolder.text = ""
+        print("SHOW possible drinks: ", masterTextList)
+        if(masterTextList.contains("vodka") && masterTextList.contains("lime")) {
+            textHolder.text = textHolder.text! + "VODKA + LIME "
+        }
+        if(masterTextList.contains("tequila") && masterTextList.contains("lime")) {
+            textHolder.text = textHolder.text! + "Tequila + LIME "
+        }
+        if(masterTextList.contains("rum") && masterTextList.contains("coke")) {
+            textHolder.text = textHolder.text! + "rum + coke "
+        }
+        if(masterTextList.contains("tequila") && masterTextList.contains("sprite")) {
+            textHolder.text = textHolder.text! + "tequila + Sprite "
+        }
+        if(masterTextList.contains("red bull") && masterTextList.contains("vodka")) {
+            textHolder.text = textHolder.text! + "VODKA + redbull "
+        }
+        if(masterTextList.contains("peppermint") && masterTextList.contains("lime")) {
+            textHolder.text = textHolder.text! + "peppermint + LIME "
+        }
+        
+        
+    }
+    
+   
+}
---
+++ Speech-Recognition-Demo/Speech-Recognition-Demo/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Speech-Recognition-Demo
+//
+//  Created by Igo, Paul on 10/24/19.
+//  Copyright © 2019 Jennifer A Sipila. All rights reserved.
+//
+
+import Foundation
+
+
+
